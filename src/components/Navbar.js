e849c99a54ca28ea7163b022b1f180fb01cca407
Chindalath <chindalatht@gmail.com>
2021-06-04 12:06:23 -0700

    fixes linking issue and comments todos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,27 +14,38 @@ function Navbar() {
 	// 	}
 	// }
 
+	// function menuToggle() {
+	// 	if menu (is active) {
+	// 		show menu
+	// 	}
+	// 	show menu icon
+	// }
+
 	return (
 		<>
 			<nav className="navbar">
 				<div className="navbar__container">
+					{/* todo: might have to change the Link component to NavLink */}
 					<Link to="/" className="navbar__logo">
 						<img width="40" height="40" alt="reading glasses" src={logo_dark} />
          			</Link>
-					<img class="menu-bar" src={menu} alt="menu bar" />
+					<img className="menu-bar" src={menu} alt="menu bar" />
 					<ul className="navbar__menu">
 						<li className="navbar__item">
-							<Link to="/about" className="navbar__link" />
-							About
+							<Link to="/about" className="navbar__link" >
+								About
+							</Link>
 						</li>
 						<li className="navbar__item">
-							<Link to="/resume" className="navbar__link" />
-							Resume
+							<Link to="/resume" className="navbar__link">
+								Resume
+							</Link>
 						</li>
 						<li className="navbar__item">
 							{/* todo: link below will probably not be a link component since it navigates off the website */}
-							<Link to="/blog" className="navbar__link" />
-							Blog
+							<a href="https://chindalatht.medium.com/" className="navbar__link" target="blank">
+								Blog
+							</a>
 						</li>
 					</ul>
 				</div>
