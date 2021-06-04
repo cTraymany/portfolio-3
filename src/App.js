75ebd594c564ca15078ead3de1419a8e620cfb49
Chindalath <chindalatht@gmail.com>
2021-06-04 12:47:04 -0700

    placeholder for importing footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+// import Footer from './components/Footer'
 
 function App() {
   return (
