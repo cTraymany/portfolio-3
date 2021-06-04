08a3adc0cce11b5d746d462caec111f3c1e0a9b9
Chindalath <chindalatht@gmail.com>
2021-06-04 12:06:36 -0700

    creates About component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import about_me from '../images/about_me'
+
+export default function About() {
+    return (
+        <>
+            <img src={about_me} alt="Chindalath and her cat" />
+            "Hi! I'm Chindalath"
+        </>
+    )
+}
