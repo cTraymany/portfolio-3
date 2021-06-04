74d9d389de5205cbaafb3883e827a6c19deb7f78
Chindalath <chindalatht@gmail.com>
2021-06-03 19:26:35 -0700

    adds subtitle to hero and nav button

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ export default function Home() {
         <>
             <img src={plants} alt="plants" />
             <h1>Chindalath Traymany</h1>
+            <h2><b>Fullstack Engineer</b>   |   Ruby, Rails, JavaScript, React.js</h2>
+            {/* turn this upsidedown and enlarge lol */} ^
         </>
     )
 }
