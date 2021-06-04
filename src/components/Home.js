820ae805949629aed04e42be1ef2b36f1713d1d4
Chindalath <chindalatht@gmail.com>
2021-06-03 19:28:44 -0700

    todo comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
             <img src={plants} alt="plants" />
             <h1>Chindalath Traymany</h1>
             <h2><b>Fullstack Engineer</b>   |   Ruby, Rails, JavaScript, React.js</h2>
-            {/* turn this upsidedown and enlarge lol */} ^
+            {/* todo: turn this upsidedown and enlarge lol */} ^
         </>
     )
 }
