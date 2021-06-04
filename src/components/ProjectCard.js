d90975135154da388eaf5332c52cab3844f0439a
Chindalath <chindalatht@gmail.com>
2021-06-04 15:12:22 -0700

    renders image, title, and description of project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,9 @@
-export default function ProjectCard() {
+export default function ProjectCard({project}) {
     return(
         <>
-            <p>Project Card</p>
+            <img src={project.image} alt={project.alt} />
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
         </>
     )
 }
