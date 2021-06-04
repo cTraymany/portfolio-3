7b3004ae301301b2def1c21da295507084af6d15
Chindalath <chindalatht@gmail.com>
2021-06-04 15:11:51 -0700

    adds break for visibility

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
             <img src={plants} alt="plants" />
             <h1>Chindalath Traymany</h1>
             <h2><b>Fullstack Engineer</b>   |   Ruby, Rails, JavaScript, React.js</h2>
-            {/* todo: turn this upsidedown and enlarge lol */} ^
+            {/* todo: turn this upsidedown and enlarge lol */} ^ <br />
             <Projects />
         </>
     )
