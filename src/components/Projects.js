7e87748138bcd2f92adc13296dc4e80854cb9ae2
Chindalath <chindalatht@gmail.com>
2021-06-03 19:01:08 -0700

    creates projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import ProjectCard from './ProjectCard'
+
+export default function Projects() {
+    return(
+        <>
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+        </>
+    )
+}
