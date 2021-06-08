024898b5aa7400a0c49da1c8382ecffa743248fb
Chindalath <chindalatht@gmail.com>
2021-06-08 12:53:45 -0700

    moves functions to higher level components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import Home from './components/Home'
 import About from './components/About'
 import Footer from './components/Footer'
 
 function App() {
+  const [logoColor, setLogoColor] = useState("#FEFEFE")
+
+	useEffect(() => {
+		// todo: color doesn't change because component is not being re-rendered.
+		// component is not being re-rendered because there is no change;
+		// the change is within the path, and not the component
+
+    // moved from nav component so I could add onchange to routes
+		switch (window.location.pathname) {
+			case "/about":
+				setLogoColor("#636363")
+				return logoColor
+			default:
+				setLogoColor("#FEFEFE")
+				return logoColor
+		}
+	}, [logoColor])
+
   return (
     <Router>
-      <Navbar />
+      <Navbar logoColor={logoColor} />
       <Switch>
-        <Route path="/about" exact>
+        <Route path="/about" exact onChange={() => console.log("hi")}>
           <About />
         </Route>
-        <Route path="/" >
+        <Route path="/" onChange={() => console.log("hi")}>
           <Home />
         </Route>
       </Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,13 @@
 import './Navbar.css';
 // useState to toggle nav icons and change icon colors
-import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 // import logo from '../images/logo.svg';
 import { ReactComponent as Menu } from '../images/menu.svg'
 import { ReactComponent as Close} from '../images/close.svg'
 import { ReactComponent as Logo } from '../images/logo.svg';
 
-function Navbar() {
-	const [logoColor, setLogoColor] = useState("white")
+function Navbar({logoColor}) {
 
-	useEffect(() => {
-		// todo: color doesn't change because component is not being re-rendered.
-		// component is not being re-rendered because there is no change;
-		// the change is within the path, and not the component
-		switch (window.location.pathname) {
-			case "/about":
-				setLogoColor("black")
-				return logoColor
-			default:
-				setLogoColor("white")
-				return logoColor
-		}
-	}, [logoColor])
 
 	// function menuToggle() {
 	// 	if menu (is active) {
