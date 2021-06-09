0e9f94fb06b38e89644cd69323ac79c74438b5c9
Chindalath <chindalatht@gmail.com>
2021-06-08 19:01:45 -0700

    most usestate and useeffect back to nav component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { useLocation } from 'react-router'
-import React, { useState, useEffect } from 'react';
 import Home from './components/Home'
 import About from './components/About'
 import Footer from './components/Footer'
 
 function App() {
-  const [logoColor, setLogoColor] = useState("#FEFEFE")
-  const location = useLocation()
-
-	useEffect(() => {
-		// todo: color doesn't change because component is not being re-rendered.
-		// component is not being re-rendered because there is no change;
-		// the change is within the path, and not the component
-
-    // moved from nav component so I could add onchange to routes
-		switch (location.pathname) {
-			case "/about":
-				setLogoColor("#636363")
-				return logoColor
-			default:
-				setLogoColor("#FEFEFE")
-				return logoColor
-		}
-	}, [logoColor, location])
-
   return (
     <Router>
-      <Navbar logoColor={logoColor} />
+      <Navbar />
       <Switch>
-        <Route path="/about" exact onChange={() => console.log("hi")}>
+        <Route path="/about" exact>
           <About />
         </Route>
-        <Route path="/" onChange={() => console.log("hi")}>
+        <Route path="/">
           <Home />
         </Route>
       </Switch>
