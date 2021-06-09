c5ad3f8259fcf2079d4ca43c10df6ef4082d5cef
Chindalath <chindalatht@gmail.com>
2021-06-08 17:23:39 -0700

    adds classnames to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,15 @@ import Projects from './Projects.js'
 export default function Home() {
 
     return (
-        <>
-            <img src={plants} alt="plants" />
-            <h1>Chindalath Traymany</h1>
-            <h2><b>Fullstack Engineer</b>   |   Ruby, Rails, JavaScript, React.js</h2>
-            {/* todo: turn this upsidedown and enlarge lol */}
-            <div>^</div>
-            <Projects />
-        </>
+        <div className="hero">
+            <div className="hero__container">
+                <img className="hero__image" src={plants} alt="plants" />
+                <h1 className="hero__title">Chindalath Traymany</h1>
+                <h2 className="hero__subtitle"><b>Fullstack Engineer</b>   |   Ruby, Rails, JavaScript, React.js</h2>
+                {/* todo: turn this upsidedown and enlarge lol */}
+                <div>^</div>
+                <Projects />
+            </div>
+        </div>
     )
 }
