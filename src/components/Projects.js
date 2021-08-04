fef50a72bd387211d44ba726020e338f7bcde11a
Chindalath <chindalatht@gmail.com>
2021-08-04 16:27:28 -0700

    adds programming languages used to ui and github links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,17 +10,23 @@ export default function Projects() {
             title: "Problem Solver",
             description: "Problem Solver has a custom API containing user-submitted problems and solutions. The API is generated with the Fast JSON gem to accelerate serialization. The JavaScript frontend creates and displays the application in a single, dynamic page, and communicates with the Rails backend models and controllers to parse and validate problem/solution objects.",
             image: problem_solver,
-            alt: "problem solver"
+            alt: "problem solver",
+            languages: ["Node", "Rails", "CSS"],
+            github: "https://github.com/cTraymany/js-problem-solver"
         },{
             title: "Social Media Distancing",
             description: "This Ruby on Rails web app allows users to break unhealthy social media habits through creating goals that focus on themselves. Register safely with Bcrypt encrypted passwords and start journaling your aspirations! Or sign in with your Google account for a faster experience provided by omniauth. Journeys and goals are stored in a SQLite3 database.",
             image: social_media_distancing,
-            alt: "social media distancing"
+            alt: "social media distancing",
+            languages: ["Rails", "SQLite3"],
+            github: "https://github.com/cTraymany/Rails-Social-Media-Distancing"
         },{
             title: "Around the World",
             description: "Use this simple CLI application to Learn about a new country! Search by calling-code to view 5 data points per country gathered through scraping REST Countries API. Each country’s data is parsed to retrieve nested information for display using Ruby.",
             image: country_cli,
-            alt: "around the world"
+            alt: "around the world",
+            languages: ["Ruby"],
+            github: "https://github.com/cTraymany/country-cli"
         }
     ]
 
