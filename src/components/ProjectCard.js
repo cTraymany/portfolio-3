5b02db4ef113a72640fd17a2a882e59be5ba74de
Chindalath <chindalatht@gmail.com>
2021-08-03 17:33:37 -0700

    code cleanup and styles projects component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,10 @@
 export default function ProjectCard({project}) {
     return(
         <div className="project">
-            <div className="project__image">
+            <div className="project--image">
                 <img src={project.image} alt={project.alt} />
             </div>
-            <div className="project__description">
+            <div className="project--description">
                 <h3>{project.title}</h3>
                 <p>{project.description}</p>
             </div>
