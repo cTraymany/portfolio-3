33cdd8bc43dae88f9cd199cfbc4bb84368cda235
Chindalath <chindalatht@gmail.com>
2021-08-25 12:06:36 -0700

    rewrites mobile navbar

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,58 +1,50 @@
 import { Link } from 'react-router-dom';
-import { ReactComponent as Logo } from '../images/logo.svg';
-import { ReactComponent as Hamburger } from '../images/menu.svg'
-import { ReactComponent as Close } from '../images/close.svg'
-import React, { useState } from 'react';
 
-function MobileNavbar({ location, logoColor }) {
+function MobileNavbar({ location }) {
 
-    const [menuClass, setMenuClass] = useState(false)
-    const [sidebarClass, setSidebarClass] = useState(true)
+    // const handleClick = (event) => {
+    //     // select menu; on click of any links, set menu html to "unchecked"
+    // }
 
-    const handleClick = (event) => {
-        setMenuClass(!menuClass)
-        setSidebarClass(!sidebarClass)
-        console.log(menuClass)
-    }
-
-	return (
-		<nav className={"mobile-navbar__container " + (location.pathname === "/about" ? "mobile-navbar__container--about" : "mobile-navbar__container--home")}>
-			<Link to="/" className="mobile-navbar__logo">
-				<Logo fill={logoColor} />
-			</Link>
-			<Hamburger fill={logoColor} className={`mobile-navbar__button--hamburger ${!menuClass ? "active" : "inactive"}`} onClick={handleClick} />
-			<Close fill={logoColor} className={`mobile-navbar__button--close ${!menuClass ? "inactive" : "active"}`} onClick={handleClick} />
-			<div className={`mobile-navbar__menu ${!sidebarClass ? "active" : "inactive"}`} >
-				<ul className="mobile-navbar__menu--list">
-					<li className="mobile-navbar__menu--item">
-						<Link to="/" className="mobile-navbar__menu--link">
+    return (
+        <div className={"mobile-navigation " + (location.pathname === "/about" ? "mobile-navigation--about" : "mobile-navigation--home")}>
+            <input type="checkbox" className="mobile-navigation__checkbox" id="navi-toggle" />
+            <label htmlFor="navi-toggle" className="mobile-navigation__button">
+                <span className="mobile-navigation__icon">&nbsp;</span>
+            </label>
+            <div className="mobile-navigation__background">&nbsp;</div>
+            <nav className="mobile-navigation__nav">
+                <ul className="mobile-navigation__list">
+                    <li className="mobile-navigation__item">
+                        <Link to="/" className="mobile-navigation__link">
 							Home
-						</Link>
-					</li>
-					<li className="mobile-navbar__menu--item">
-						<Link to="/about" className="mobile-navbar__menu--link">
+						</Link>    
+                    </li>
+                    <li className="mobile-navigation__item">
+                        <Link to="/about" className="mobile-navigation__link">
 							About
 						</Link>
-					</li>
-					<li className="mobile-navbar__menu--item">
-						<a href="https://docs.google.com/document/d/1TePmZMUAlMTyqBuH83UNYv3Xugh1QIF8TziIAMLBYWw/edit?usp=sharing" className="mobile-navbar__menu--link" target="blank">
+                    </li>
+
+                    <li className="mobile-navigation__item">
+						<a href="https://docs.google.com/document/d/1TePmZMUAlMTyqBuH83UNYv3Xugh1QIF8TziIAMLBYWw/edit?usp=sharing" className="mobile-navigation__link" target="blank">
 							Resume
 						</a>
 					</li>
-					<li className="mobile-navbar__menu--item">
-						<a href="https://www.linkedin.com/in/chindalath/" className="mobile-navbar__menu--link" target="blank">
+					<li className="mobile-navigation__item">
+						<a href="https://www.linkedin.com/in/chindalath/" className="mobile-navigation__link" target="blank">
 							LinkedIn
 						</a>
 					</li>
-					<li className="mobile-navbar__menu--item">
-						<a href="https://chindalatht.medium.com/" className="mobile-navbar__menu--link" target="blank">
+					<li className="mobile-navigation__item">
+						<a href="https://chindalatht.medium.com/" className="mobile-navigation__link" target="blank">
 							Blog
 						</a>
 					</li>
-				</ul>
-			</div>
-		</nav>
-	)
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default MobileNavbar
