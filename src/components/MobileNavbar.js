71aa3614f67cf87f132c7e9b4d79c03c27654a15
Chindalath <chindalatht@gmail.com>
2021-08-25 20:22:38 -0700

    menu closes when link is clicked

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -2,9 +2,10 @@ import { Link } from 'react-router-dom';
 
 function MobileNavbar({ location }) {
 
-    // const handleClick = (event) => {
-    //     // select menu; on click of any links, set menu html to "unchecked"
-    // }
+    const handleClick = (event) => {
+        const checkbox = document.getElementById("navi-toggle");
+        checkbox.checked = false;
+    }
 
     return (
         <div className={"mobile-navigation " + (location.pathname === "/about" ? "mobile-navigation--about" : "mobile-navigation--home")}>
@@ -16,28 +17,28 @@ function MobileNavbar({ location }) {
             <nav className="mobile-navigation__nav">
                 <ul className="mobile-navigation__list">
                     <li className="mobile-navigation__item">
-                        <Link to="/" className="mobile-navigation__link">
+                        <Link to="/" className="mobile-navigation__link" onClick={handleClick}>
 							Home
 						</Link>    
                     </li>
                     <li className="mobile-navigation__item">
-                        <Link to="/about" className="mobile-navigation__link">
+                        <Link to="/about" className="mobile-navigation__link" onClick={handleClick}>
 							About
 						</Link>
                     </li>
 
                     <li className="mobile-navigation__item">
-						<a href="https://docs.google.com/document/d/1TePmZMUAlMTyqBuH83UNYv3Xugh1QIF8TziIAMLBYWw/edit?usp=sharing" className="mobile-navigation__link" target="blank">
+						<a href="https://docs.google.com/document/d/1TePmZMUAlMTyqBuH83UNYv3Xugh1QIF8TziIAMLBYWw/edit?usp=sharing" className="mobile-navigation__link" target="blank" onClick={handleClick}>
 							Resume
 						</a>
 					</li>
 					<li className="mobile-navigation__item">
-						<a href="https://www.linkedin.com/in/chindalath/" className="mobile-navigation__link" target="blank">
+						<a href="https://www.linkedin.com/in/chindalath/" className="mobile-navigation__link" target="blank" onClick={handleClick}>
 							LinkedIn
 						</a>
 					</li>
 					<li className="mobile-navigation__item">
-						<a href="https://chindalatht.medium.com/" className="mobile-navigation__link" target="blank">
+						<a href="https://chindalatht.medium.com/" className="mobile-navigation__link" target="blank" onClick={handleClick}>
 							Blog
 						</a>
 					</li>
