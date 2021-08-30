48aaab127c3472050f2a0caaef70b90f4a004a35
Chindalath <chindalatht@gmail.com>
2021-08-29 17:59:11 -0700

    styles about and minor styling changes to other components

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,26 @@
 
 export default function About() {
     return (
-        <div className="u-align">    
-            <main className="about">
-                {/* <img src={hero} alt="Chindalath and her cat" className="about__image" /> */}
-                <div className="about__text">
-                    <h1 className="about__text--primary">Hi! I'm Chindalath :)</h1>
-                    <p className="about__text--secondary">I am a Laotian-American woman, a software developer, and a lifelong learner.</p><br />
-                    <p className="about__text--secondary">I have a background in retail and food service management as well as healthcare administration. Being surrounded by the infinitely expanding tech scene, San Francisco played a role in my decision to transition to software engineering to pursue my passion for mentally stimulating and complex problem-solving.</p><br />
-                    <p className="about__text--secondary">I am a graduate of Flatiron School where I studied JavaScript, Ruby, HTML, CSS, RESTful APIs, software design patterns such as MVC, and a myriad of libraries and frameworks including Rails, Sinatra, React, and Redux. I like to build on my technical knowledge and dip my toes in several other learning opportunities that are available to me through the vast free resources of the internet.</p><br />
-                    <p className="about__text--secondary">I chose to become a software developer, and with this choice, I am more excited for the future than ever.</p><br />
+        <div className="about">    
+            <div className="about__content">
+                <div className="about__left">
+                    <h1 className="about__heading--mobile">About Me</h1>
                 </div>
-            </main>
+                <div className="about__right">
+                    <h3 className="about__heading">Hi! I'm Chindalath :)</h3>
+                    <div className="about__text">
+                        <p>
+                            I am a Laotian-American woman, a software developer, and a lifelong learner.
+                            I have a background in retail and food service management as well as healthcare administration.
+                            Being surrounded by the infinitely expanding tech scene, San Francisco played a role in my decision to transition to software engineering to pursue my passion for mentally stimulating and complex problem-solving.
+                        </p>
+                        <p>
+                            I am a graduate of Flatiron School where I studied JavaScript, Ruby, HTML, CSS, RESTful APIs, software design patterns such as MVC, and a myriad of libraries and frameworks including Rails, Sinatra, React, and Redux. I like to build on my technical knowledge and dip my toes in several other learning opportunities that are available to me through the vast free resources of the internet.
+                            I chose to become a software developer, and with this choice, I am more excited for the future than ever.
+                        </p>
+                    </div>
+                </div> 
+            </div>
         </div>
     )
 }
