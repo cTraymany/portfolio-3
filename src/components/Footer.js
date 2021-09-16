691ae2c2f8a358dfce141dbbe98984d423878750
Chindalath <chindalatht@gmail.com>
2021-09-16 14:01:22 -0700

    footer message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
         <footer className="footer">
-            <small>Like what you see? <b>chindalatht@gmail.com</b></small>
+            <small>Created by Chindalath in San Francisco, CA @2021 &#124; <b>chindalatht@gmail.com</b></small>
         </footer>
     )
 }
